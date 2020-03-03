043e8ed7ffc321d852cc185ab56d39c99b590fc5
yggdevs <yarey_garcia@hotmail.com>
2020-03-03 13:24:57 +0000

    Signed-off-by: yggdevs <yarey_garcia@hotmail.com>

--- Control TempHum/Form1.Designer.cs
+++ Control TempHum/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace Control_TempHum
+﻿using System;
+using System.IO.Ports;
+
+namespace Control_TempHum
 {
     partial class Form1
     {
@@ -34,80 +37,99 @@
             this.TextBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(135, 161);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(27, 126);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(74, 26);
+            this.button1.Size = new System.Drawing.Size(111, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Enviar";
+            this.button1.Text = "Abrir puerto";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(304, 161);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(576, 42);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(71, 26);
+            this.button2.Size = new System.Drawing.Size(145, 40);
             this.button2.TabIndex = 1;
-            this.button2.Text = "Recibir";
+            this.button2.Text = "Recibir datos";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // TextBox1
             // 
-            this.TextBox1.Location = new System.Drawing.Point(182, 51);
-            this.TextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TextBox1.Location = new System.Drawing.Point(62, 42);
             this.TextBox1.Name = "TextBox1";
-            this.TextBox1.Size = new System.Drawing.Size(136, 20);
+            this.TextBox1.Size = new System.Drawing.Size(202, 26);
             this.TextBox1.TabIndex = 2;
             this.TextBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(182, 84);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(350, 135);
+            this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(136, 20);
+            this.textBox2.Size = new System.Drawing.Size(404, 60);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // serialPort1
             // 
             this.serialPort1.PortName = "COM4";
-            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
             // 
-            // label1
+            // textBox3
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(339, 90);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(90, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Punto de rocio Cº";
-            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            this.textBox3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(350, 228);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(404, 58);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(350, 321);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(404, 65);
+            this.textBox4.TabIndex = 5;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(533, 292);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(800, 449);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.TextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
+
+        private SerialDataReceivedEventHandler GetSerialPort1_DataReceived()
+        {
+            throw new NotImplementedException();
+        }
+
+
+
 
         #endregion
 
@@ -115,8 +137,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox TextBox1;
         private System.Windows.Forms.TextBox textBox2;
-        private System.IO.Ports.SerialPort serialPort1;
-        private System.Windows.Forms.Label label1;
+        private SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- Control TempHum/Form1.cs
+++ Control TempHum/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Control_TempHum
 {
@@ -14,65 +8,153 @@ namespace Control_TempHum
     {
         private const string close = "Port is close";
         public String Data;
-
+       
 
         public Form1()
         {
             InitializeComponent();
+
         }
 
-       
+   
+
+     
+
 
         private void Button1_Click(object sender, EventArgs e)
         {
             if (!serialPort1.IsOpen)
             {
                 serialPort1.Open();
+                Data = serialPort1.ReadLine();
+                
+
                 Console.WriteLine("puerto abierto");
 
+
             }
-            else {
+            else
+            {
                 Console.WriteLine(close);
             }
         }
+
+
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
-        private void TextBox2_TextChanged(object sender, EventArgs e)
+        private void TextBox2(object sender, EventArgs e)
         {
 
-        }
-
-        private void SerialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
-        {
-         Data=serialPort1.ReadLine();
-            Console.WriteLine("Datos recibidos");
-            Console.WriteLine(Data);
-            Console.WriteLine("****");
 
 
         }
 
 
 
-        private void Button2_Click(object sender, EventArgs e)
+
+        public void Button2_Click(object sender, EventArgs e)
         {
+          
 
-                textBox2.Text = Data;
-                // Data = serialPort1.ReadLine();
 
-                Console.WriteLine("Datos de boton2");
-                Console.WriteLine(Data);
-            
+            int count = 3;
+            // Crear vector con tamaño que se ha asignado
+            string[] datossensor = new string[count];
+
+            // Llenar 
+
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("dato de sensor {0}: ", i);
+                datossensor[i] = serialPort1.ReadLine();
+                
+            }
+            textBox2.Text = datossensor[0];
+            textBox3.Text = datossensor[1];
+            textBox4.Text = datossensor[2];
+
+
+
+            // Imprimirlo
+            Console.WriteLine("\nValores del vector:\n");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Posicion {0}: {1}", i, datossensor[i]);
+             
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
         }
+
+
+
+
+
 
 
 
 
 
         private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //DisplayText sirve para ver los datos en tiempo real
+        private void DisplayText(object sender, EventArgs e)
+        {
+
+
+            textBox2.AppendText(Data);
+           
+
+        }
+
+        
+
+
+
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
