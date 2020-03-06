b738a1e83eab92a0c8c1a13222973b9a4f04211b
yggdevs <yarey_garcia@hotmail.com>
2020-03-06 12:56:09 +0000

    Añadido conectarpuertos automaticamente,log de temperatura y humedad

--- Control TempHum/Form1.Designer.cs
+++ Control TempHum/Form1.Designer.cs
@@ -32,24 +32,14 @@ namespace Control_TempHum
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.TextBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(27, 126);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 40);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Abrir puerto";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // button2
             // 
@@ -60,14 +50,6 @@ namespace Control_TempHum
             this.button2.Text = "Recibir datos";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
-            // 
-            // TextBox1
-            // 
-            this.TextBox1.Location = new System.Drawing.Point(62, 42);
-            this.TextBox1.Name = "TextBox1";
-            this.TextBox1.Size = new System.Drawing.Size(202, 26);
-            this.TextBox1.TabIndex = 2;
-            this.TextBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -83,6 +65,7 @@ namespace Control_TempHum
             // serialPort1
             // 
             this.serialPort1.PortName = "COM4";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
             // 
             // textBox3
             // 
@@ -104,19 +87,39 @@ namespace Control_TempHum
             this.textBox4.Size = new System.Drawing.Size(404, 65);
             this.textBox4.TabIndex = 5;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(35, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(141, 28);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Conectar puerto";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 449);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.TextBox1);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -132,14 +135,13 @@ namespace Control_TempHum
 
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox TextBox1;
         private System.Windows.Forms.TextBox textBox2;
         private SerialPort serialPort1;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Control TempHum/Form1.cs
+++ Control TempHum/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.IO;
 
 namespace Control_TempHum
 {
@@ -13,33 +14,104 @@ namespace Control_TempHum
         public Form1()
         {
             InitializeComponent();
+            conectarpuertos();
+            
+
 
         }
 
-   
-
-     
 
 
-        private void Button1_Click(object sender, EventArgs e)
+        public void conectarpuertos() {
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            foreach (string s in SerialPort.GetPortNames())
+
+            {
+
+                comboBox1.Items.Add(s);
+
+            }
+
+        }
+
+
+
+        public void generartxt(string datossensor0, string datossensor1, string datossensor2)
         {
-            if (!serialPort1.IsOpen)
+
+            string time = DateTime.Now.ToString("hh:mm:ss");
+            string date = DateTime.Now.ToLongDateString();
+
+
+          
+            // timer para que se ejecute a una hora determinada para obtener los datos solo las horas que indicamos
+         /*   
+            for (int i = 10; i < 12; i++)
             {
-                serialPort1.Open();
-                Data = serialPort1.ReadLine();
+                int I = i;
+              
+              
+                if (time.StartsWith(I.ToString()))
+                {
+
+                    Console.Write("funcioina  " + time+"  iterador  "+I);
+                }
+            }
+
+               */
+          
+
+
+                string path = @"c:\temp\Control_TEM_HUMEDAD.txt";
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+
+               
+                  
+                try
+                {
+                    sw.WriteLine("Fecha " + date+ " time  " +time+"  " +datossensor0+" "+ datossensor1+" " + datossensor2);
+                   
+                }
+                catch {
+                    sw.WriteLine("No hay datos");
+                }  
+
                 
+                }
+            
 
-                Console.WriteLine("puerto abierto");
 
 
-            }
-            else
-            {
-                Console.WriteLine(close);
-            }
         }
 
 
+      /*   private void Button1_Click(object sender, EventArgs e)
+        {
+
+           
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("The following serial ports were found:");
+
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+               // textBox5.Text = "Estas conectado al " +port;
+               
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+    */
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
         {
@@ -57,39 +129,43 @@ namespace Control_TempHum
 
         public void Button2_Click(object sender, EventArgs e)
         {
-          
+            int c = 0;
+            int d = 1;
+
+            while (c < d) {
+                int count = 3;
+                // creamo sun vector de tamaño 3 porque vamos a recibir tres datos
+                string[] datossensor = new string[count];
+
+                // Llenar 
 
 
-            int count = 3;
-            // Crear vector con tamaño que se ha asignado
-            string[] datossensor = new string[count];
-
-            // Llenar 
-
-
-            for (int i = 0; i < 3; i++)
-            {
-                Console.Write("dato de sensor {0}: ", i);
-                datossensor[i] = serialPort1.ReadLine();
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write("dato de sensor {0}: ", i);
+                    datossensor[i] = serialPort1.ReadLine();
+                   
+                }
                 
+
+
+
+                // Imprimirlo
+                Console.WriteLine("\nValores del vector:\n");
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Posicion {0}: {1}", i, datossensor[i]);
+                  
+
+                    textBox2.Text = datossensor[0];
+                    textBox3.Text = datossensor[1];
+                    textBox4.Text = datossensor[2];
+                    generartxt(datossensor[0], datossensor[1], datossensor[2]);
+
+                }
+               
+
             }
-            textBox2.Text = datossensor[0];
-            textBox3.Text = datossensor[1];
-            textBox4.Text = datossensor[2];
-
-
-
-            // Imprimirlo
-            Console.WriteLine("\nValores del vector:\n");
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("Posicion {0}: {1}", i, datossensor[i]);
-             
-
-            }
-
-
-
 
 
 
@@ -116,24 +192,6 @@ namespace Control_TempHum
         }
 
 
-        //DisplayText sirve para ver los datos en tiempo real
-        private void DisplayText(object sender, EventArgs e)
-        {
-
-
-            textBox2.AppendText(Data);
-           
-
-        }
-
-        
-
-
-
-
-
-
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -155,6 +213,51 @@ namespace Control_TempHum
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //cerramos el puerto cuando cerramos el formulario
+            serialPort1.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+
+        {
+
+           // este evento SelectedIndexChanged.
+            //hará que el puerto sea válido deshabilitará el resto .
+
+            serialPort1.PortName = comboBox1.Text;
+
+            try
+            {
+
+                serialPort1.Open();
+                
+            }
+
+            catch
+
+            {
+
+                MessageBox.Show("Puerto no válido");
+
+                return;
+
+            }
+
+            comboBox1.Enabled = false;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+          
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
         {
 
         }
